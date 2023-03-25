26ab33f2edc1e0cd5b4dfbf034d87b9dba8abb89
JeongHaeChep <eiop77@naver.com>
2023-03-26 00:28:30 +0900

    init

---
+++ src/test/java/com/example/swimranking/SwimRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swimranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
